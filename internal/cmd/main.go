71139fe5238fa0109de4518bd9b02695ba3c2631
Toytum <steath1999@gmail.com>
2019-11-07 14:05:47 -0800

    normalize directory structure

---
+++ internal/cmd/main.go
@@ -0,0 +1,77 @@
+package main
+
+import "fmt"
+
+func main() {
+	printWelcome()
+	// avoid temporary variables
+	// name := getName()
+	printGreeting(responseToPrompt())
+	fmt.Println("Let's go on an adventure!")
+	travel()
+}
+
+func printWelcome() {
+	fmt.Println("Welcome to the Solar System!")
+	fmt.Println("There are 8 planets to explore.")
+	fmt.Println("What is your name?")
+}
+
+func responseToPrompt() string {
+	var response string
+	fmt.Scan(&response)
+	return response
+}
+
+func printGreeting(name string) {
+	fmt.Printf("Nice to meet you, %s. My name is Eliza, I'm an old friend of Siri.\n", name)
+
+}
+
+func travel() {
+	var choice string
+	fmt.Println("Shall I randomly choose a planet for you to visit? (Y or N)")
+	for choice != "Y" && choice != "N" {
+		choice = responseToPrompt()
+		if choice == "Y" {
+			travelToRandomPlanet()
+		} else if choice == "N" {
+			fmt.Println("Name the planet you would like to visit.")
+			planetName := responseToPrompt()
+			travelToPlanet(planetName)
+		} else {
+			fmt.Println("Sorry, I didn't get that.")
+		}
+	}
+}
+
+func travelToRandomPlanet() {
+	fmt.Println("Traveling to Jupiter...")
+	fmt.Println("Arrived at Jupiter. The large red spot appears ominous.")
+}
+
+func travelToPlanet(planetName string) {
+	fmt.Printf("Traveling to %s...\n", planetName)
+	fmt.Println("Arrived at Neptune. A very cold planet, furthest from the sun.")
+}
+
+// func getName() string {
+// 	var name string
+// 	fmt.Println("What is your name?")
+// 	fmt.Scan(&name)
+// 	return name
+// }
+
+// func getTravelChoice() string {
+// 	var choice string
+// 	fmt.Println("Shall I randomly choose a planet for you to visit? (Y or N)")
+// 	fmt.Scan(&choice)
+// 	return choice
+// }
+
+// func getPlanetName() string {
+// 	var name string
+// 	fmt.Println("Name the planet you would like to visit.")
+// 	fmt.Scan(&name)
+// 	return name
+// }
